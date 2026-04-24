1766a6030dd46627598c65424133e18ceeafdea8
yuri <soothe7716@gmail.com>
2026-04-24 22:47:47 +0900

    fix: robots.tsの追加と本番への反映

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,21 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: "/admin/",
-    },
+    rules: [
+      {
+        userAgent: "facebookexternalhit",
+        allow: "/",
+      },
+      {
+        userAgent: "Twitterbot",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: "/admin/",
+      },
+    ],
     sitemap: "https://famous-quote-bottle-message.vercel.app/sitemap.xml",
   };
 }
